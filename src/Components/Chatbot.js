c4e70f873291056337e871904c8747e6912dbeaa
imani.devs <imaniroberts.dev@gmail.com>
2023-04-17 21:44:49 -0400

    add Chatbot function

---
+++ src/Components/Chatbot.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ChatBot from 'react-simple-chatbot';
+
+const Chatbot = () => {
+	const steps = [
+		{
+			id: '0',
+			message: 'Welcome to react chatbot!',
+			trigger: '1'
+		},
+		{
+			id: '1',
+			message: 'Bye!',
+			end: true
+		}
+	];
+
+	return (
+		<div>
+			<ChatBot steps={steps} />
+		</div>
+	);
+};
+
+export default Chatbot;
