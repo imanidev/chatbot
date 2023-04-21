5f14762edd664c716a8ae1bd4794b40a1f785eb6
imani.devs <imaniroberts.dev@gmail.com>
2023-04-20 23:04:51 -0400

    changed steps

--- src/Components/Chatbot.js
+++ src/Components/Chatbot.js
@@ -5,53 +5,43 @@ const Chatbot = (index) => {
 	const steps = [
 		{
 			id: '0',
-			message: 'Welcome to our Customer Service Chatbot!',
+			message: "Hi, I'm Imani!",
 			trigger: '1'
 		},
 		{
 			id: '1',
-			message: 'How can we assist you today?',
+			message: 'How can I help you today?',
 			trigger: 'options'
 		},
 		{
 			id: 'options',
 			options: [
-				{ value: 'order', label: 'Check Order Status', trigger: 'order' },
-				{ value: 'return', label: 'Return or Exchange', trigger: 'return' },
-				{ value: 'faq', label: 'Frequently Asked Questions', trigger: 'faq' },
-				{ value: 'bye', label: 'Exit', trigger: 'bye' }
-			]
+				{ value: 'joke', label: 'Tell a joke', trigger: 'joke' },
+				{ value: 'about', label: 'About Me', trigger: 'about' },
+				{ value: 'portfolio', label: 'View Portfolio', trigger: 'portfolio' },
+				{ value: 'twitter', label: 'Follow on Twitter', trigger: 'twitter' }
+			],
+			trigger: 'joke'
 		},
 		{
-			id: 'order',
-			message: 'Please provide your order number so we can check the status for you.',
-			trigger: 'orderNumber'
+			id: 'joke',
+			message: "Why did the programmer quit their job? They didn't get arrays!",
+			trigger: 'options'
 		},
 		{
-			id: 'orderNumber',
-			user: true,
-			trigger: 'checkOrderStatus'
+			id: 'about',
+			message: 'I am a full-stack developer. I love to code!',
+			trigger: 'options'
 		},
 		{
-			id: 'checkOrderStatus',
-			message: 'We are checking your order status. Please wait...'
-			// Add a function to fetch the order status, then trigger the appropriate response.
+			id: 'portfolio',
+			message: 'I am currently working on a portfolio site. Check back soon for updates!',
+			trigger: 'options'
 		},
 		{
-			id: 'return',
-			message: 'Please visit our return center at https://example.com/returns to initiate a return or exchange.',
-			trigger: '1'
-		},
-		{
-			id: 'faq',
-			message:
-				'You can find answers to frequently asked questions on our FAQ page at https://example.com/faq. If you have any other questions, feel free to ask!',
-			trigger: '1'
-		},
-		{
-			id: 'bye',
-			message: 'Thank you for using our Customer Service Chatbot. Have a great day!',
-			end: true
+			id: 'twitter',
+			message: 'Follow me on Twitter @imaniverse. See you there!',
+			trigger: 'options'
 		}
 	];
 
